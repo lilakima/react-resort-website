d7da479828af1e162d18df1a6f74f5421c086977
delila <delila@sinitic.ai>
2020-07-08 10:41:32 +0800

    Implement features link

--- src/components/Room.js
+++ src/components/Room.js
@@ -13,6 +13,9 @@ export default function Room({ room }) {
           <h6>${price}</h6>
           <p>per night</p>
         </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
       </div>
     </article>
   );
